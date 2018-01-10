d979e36dfdbbdd0602de270de04749dd91083638
Brady Berryessa <you@example.com>
2018-01-10 11:31:11 -0700

    Created partial for full coverage on growl notifications

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  before do 
+  before do
     @user = FactoryGirl.create(:user)
     login_as(@user, :scope => :user)
   end
@@ -10,6 +10,7 @@ describe 'navigate' do
     before do
       visit posts_path
     end
+
     it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
@@ -55,7 +56,6 @@ describe 'navigate' do
     end
 
     it 'can be created from new form page' do
-
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
       click_on "Save"
@@ -72,26 +72,31 @@ describe 'navigate' do
     end
   end
 
-  describe 'edit' do 
+  describe 'edit' do
     before do
-      @post = FactoryGirl.create(:post)      
-    end
-
-    it 'can be reached clicking edit on index page' do
-      visit posts_path
-
-      click_link("edit_#{@post.id}")
-      expect(page.status_code).to eq(200)
+      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      login_as(@edit_user, :scope => :user)
+      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
     end
 
     it 'can be edited' do
-      visit edit_post_path(@post)
+      visit edit_post_path(@edit_post)
 
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "Edited Content"
+      fill_in 'post[rationale]', with: "Edited content"
       click_on "Save"
 
-      expect(page).to have_content("Edited Content")
+      expect(page).to have_content("Edited content")
+    end
+
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authorized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
+
+      visit edit_post_path(@edit_post)
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
