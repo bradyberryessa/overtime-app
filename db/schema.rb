e03e7908e2f872ad28a47c031b1a56184142e8ef
Brady Berryessa <you@example.com>
2018-01-11 18:42:09 -0700

    Added hours requested to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109145454) do
+ActiveRecord::Schema.define(version: 20180112011741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.text "image"
+    t.string "subject"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.date "date"
@@ -22,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180109145454) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.integer "status", default: 0
+    t.decimal "overtime_request", default: "0.0"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
