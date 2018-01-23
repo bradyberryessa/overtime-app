c95e747f24116af410e4c2957ebb1ce9115b778c
Brady Berryessa <you@example.com>
2018-01-22 21:31:37 -0700

    Brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ AdminUser.create(email: "tester@test.com",
 puts "1 Admin User created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale contentIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
